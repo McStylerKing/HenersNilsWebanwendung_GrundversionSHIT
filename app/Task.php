0d425086c6adb50b6f9f43fd9bdfbdb724c2aa21
Nils Heners <nils.heners@web.de>
2016-04-08 10:01:01 +0200

    Grundversion

---
+++ app/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+
+
+class Task extends Model
+{
+
+    protected $fillable = ['name'];
+    
+
+    protected $casts = [
+        'user_id' => 'int',
+    ];
+
+
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
